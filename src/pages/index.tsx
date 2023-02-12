26300c3d0807771b0d514f6bcfd30742d9414956
Gustavo Cabral <studiogustavocabral@gmail.com>
2023-02-12 01:49:39 -0300

    NEW component added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,7 @@ export default function Home() {
   return (
     <div className="flex gap-8 p-4 flex-col bg-slate-100 w-full h-screen ">
   
-      <Overview/>
-      <PopularProduct/>
+    
        <SignIn/>
   
 
