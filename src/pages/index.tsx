990ca49cf3a4471847d5f45e11124e718a4ad95e
Gustavo Cabral <studiogustavocabral@gmail.com>
2023-02-12 01:53:11 -0300

    Test 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-
+import CustomerInfo from "components/CustomerInfo";
+import DropDown from "components/DropDown";
+import ItemNotification from "components/ItemNotification";
+import MessageDropdown from "components/MessageDropdown";
+import Overview from "components/Overview";
+import PopularProduct from "components/PopularProduct";
+import PriceInput from "components/PriceInput";
+import PriceInput2 from "components/PriceInput2";
+import SearchInput from "components/SearchInput";
 import SignIn from "components/SignIn";
 
 
@@ -7,8 +15,8 @@ export default function Home() {
   return (
     <div className="flex gap-8 p-4 flex-col bg-slate-100 w-full h-screen ">
   
-    
-       <SignIn/>
+      <Overview/>
+  
   
 
    
